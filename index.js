8b24f74fef8f813e73e82e011bfd819e11d1633d
EduardimGameplay <123999952+EduardimGameplay@users.noreply.github.com>
2023-03-23 21:13:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 function validarSenha() {
-    var user = document.querySelector("input[type=email]").value;
-    var senha = document.getElementById("senha").value;
-    var mensagem = document.getElementById("mensagem");
-    if (user === "eduardo" && senha === "84869")  {
+  var user = document.querySelector("input[type=email]").value;
+  var senha = document.getElementById("senha").value;
+  var mensagem = document.getElementById("mensagem");
+
+  if (user === "eduardo" && senha === "84869") {
       mensagem.style.display = "none";
       window.location.href = "login.html";
-    } else {
+  } else if (user === "joao" && senha === "12345") {
+      mensagem.style.display = "none";
+      window.location.href = "login.html";
+  } else if (user === "maria" && senha === "54321") {
+      mensagem.style.display = "none";
+      window.location.href = "login.html";
+  } else {
       mensagem.style.display = "block";
       mensagem.innerHTML = "E-mail ou senha incorretos";
-    }
   }
+}
